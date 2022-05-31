7b97fd3c1a388e6f096a1d414bd3d54871cfd0e8
M Asnan Mustakim <66906059+asnanmtakim@users.noreply.github.com>
2022-05-31 20:34:08 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+async function maubucin(){
+await swals.fire({
+  title: '<strong>Mau script bucin yang lain?</strong>',
+  html:
+    'Cek scriptnya di ' +
+    '<a href="https://bit.ly/scripthtmlbucin">https://bit.ly/scripthtmlbucin</a> ' +
+    'ya!' + '<br><br>' + 'Bisa diedit sesukamu menggunakan aplikasi Quick Editor ataupun sejenisnya.',
+  focusConfirm: false,
+});} 
